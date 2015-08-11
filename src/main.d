096046a5178d069342266db42ec0067e60d1872e
irrelevelephant <tsmithtree@gmail.com>
2015-08-11 17:06:25 -0500

    cleaning up stretching code

--- src/main.d
+++ src/main.d
@@ -1392,18 +1392,39 @@ public:
                            nframes_t localDestFrame,
                            nframes_t localEndFrame,
                            bool linkChannels = false,
-                           channels_t singleChannelIndex = 0) {
+                           channels_t singleChannelIndex = 0,
+                           AudioSequence.PieceTable leftSource = AudioSequence.PieceTable.init,
+                           AudioSequence.PieceTable rightSource = AudioSequence.PieceTable.init) {
         immutable channels_t stretchNChannels = linkChannels ? nChannels : 1;
+        immutable bool useSource = leftSource && rightSource;
+
+        auto immutable removeStartIndex = clamp(localStartFrame * nChannels,
+                                                0,
+                                                _audioSeq.currentPieceTable.logicalLength);
+        auto immutable removeEndIndex = clamp(localEndFrame * nChannels,
+                                              removeStartIndex,
+                                              _audioSeq.currentPieceTable.logicalLength);
 
         immutable double firstScaleFactor = (localSrcFrame > localStartFrame) ?
             (cast(double)(localDestFrame - localStartFrame) /
-             cast(double)(localSrcFrame - localStartFrame)) : 0;
+             cast(double)(useSource ? leftSource.length / nChannels : localSrcFrame - localStartFrame)) : 0;
         immutable double secondScaleFactor = (localEndFrame > localSrcFrame) ?
             (cast(double)(localEndFrame - localDestFrame) /
-             cast(double)(localEndFrame - localSrcFrame)) : 0;
+             cast(double)(useSource ? rightSource.length / nChannels : localEndFrame - localSrcFrame)) : 0;
 
-        uint firstHalfLength = cast(uint)(localSrcFrame - localStartFrame);
-        uint secondHalfLength = cast(uint)(localEndFrame - localSrcFrame);
+        if(useSource) {
+            localStartFrame = 0;
+            localSrcFrame = (localStartFrame < localSrcFrame) ? localSrcFrame - localStartFrame : 0;
+            localDestFrame = (localStartFrame < localDestFrame) ? localDestFrame - localStartFrame : 0;
+            localEndFrame = (localStartFrame < localEndFrame) ? localEndFrame - localStartFrame : 0;
+        }
+
+        uint firstHalfLength = cast(uint)(useSource ?
+                                          leftSource.length / nChannels :
+                                          localSrcFrame - localStartFrame);
+        uint secondHalfLength = cast(uint)(useSource ?
+                                           rightSource.length / nChannels :
+                                           localEndFrame - localSrcFrame);
         ScopedArray!(float[][]) firstHalfChannels = new float[][](stretchNChannels);
         ScopedArray!(float[][]) secondHalfChannels = new float[][](stretchNChannels);
         ScopedArray!(float*[]) firstHalfPtr = new float*[](stretchNChannels);
@@ -1417,187 +1438,48 @@ public:
             secondHalfPtr[i] = secondHalf.ptr;
         }
 
-        if(linkChannels) {
-            foreach(channels_t channelIndex, channel; firstHalfChannels) {
-                foreach(i, ref sample; channel) {
-                    sample = _audioSeq[(localStartFrame + i) * nChannels + channelIndex];
+        if(useSource) {
+            if(linkChannels) {
+                foreach(channels_t channelIndex, channel; firstHalfChannels) {
+                    foreach(i, ref sample; channel) {
+                        sample = leftSource[i * nChannels + channelIndex];
+                    }
+                }
+                foreach(channels_t channelIndex, channel; secondHalfChannels) {
+                    foreach(i, ref sample; channel) {
+                        sample = rightSource[i * nChannels + channelIndex];
+                    }
                 }
             }
-            foreach(channels_t channelIndex, channel; secondHalfChannels) {
-                foreach(i, ref sample; channel) {
-                    sample = _audioSeq[(localSrcFrame + i) * nChannels + channelIndex];
+            else {
+                foreach(i, ref sample; firstHalfChannels[0]) {
+                    sample = leftSource[i * nChannels + singleChannelIndex];
+                }
+                foreach(i, ref sample; secondHalfChannels[0]) {
+                    sample = rightSource[i * nChannels + singleChannelIndex];
                 }
             }
         }
         else {
-            foreach(i, ref sample; firstHalfChannels[0]) {
-                sample = _audioSeq[(localStartFrame + i) * nChannels + singleChannelIndex];
-            }
-            foreach(i, ref sample; secondHalfChannels[0]) {
-                sample = _audioSeq[(localSrcFrame + i) * nChannels + singleChannelIndex];
-            }
-        }
-
-        uint firstHalfOutputLength = cast(uint)(firstHalfLength * firstScaleFactor);
-        uint secondHalfOutputLength = cast(uint)(secondHalfLength * secondScaleFactor);
-        ScopedArray!(float[][]) firstHalfOutputChannels = new float[][](stretchNChannels);
-        ScopedArray!(float[][]) secondHalfOutputChannels = new float[][](stretchNChannels);
-        ScopedArray!(float*[]) firstHalfOutputPtr = new float*[](stretchNChannels);
-        ScopedArray!(float*[]) secondHalfOutputPtr = new float*[](stretchNChannels);
-        for(auto i = 0; i < stretchNChannels; ++i) {
-            float[] firstHalfOutput = new float[](firstHalfOutputLength);
-            float[] secondHalfOutput = new float[](secondHalfOutputLength);
-            firstHalfOutputChannels[i] = firstHalfOutput;
-            secondHalfOutputChannels[i] = secondHalfOutput;
-            firstHalfOutputPtr[i] = firstHalfOutput.ptr;
-            secondHalfOutputPtr[i] = secondHalfOutput.ptr;
-        }
-
-        RubberBandState rState = rubberband_new(sampleRate,
-                                                stretchNChannels,
-                                                RubberBandOption.RubberBandOptionProcessOffline,
-                                                firstScaleFactor,
-                                                1.0);
-        rubberband_set_max_process_size(rState, firstHalfLength);
-        rubberband_set_expected_input_duration(rState, firstHalfLength);
-        rubberband_study(rState, firstHalfPtr.ptr, firstHalfLength, 1);
-        rubberband_process(rState, firstHalfPtr.ptr, firstHalfLength, 1);
-        while(rubberband_available(rState) < firstHalfOutputLength) {}
-        rubberband_retrieve(rState, firstHalfOutputPtr.ptr, firstHalfOutputLength);
-        rubberband_delete(rState);
-
-        rState = rubberband_new(sampleRate,
-                                stretchNChannels,
-                                RubberBandOption.RubberBandOptionProcessOffline,
-                                secondScaleFactor,
-                                1.0);
-        rubberband_set_max_process_size(rState, secondHalfLength);
-        rubberband_set_expected_input_duration(rState, secondHalfLength);
-        rubberband_study(rState, secondHalfPtr.ptr, secondHalfLength, 1);
-        rubberband_process(rState, secondHalfPtr.ptr, secondHalfLength, 1);
-        while(rubberband_available(rState) < secondHalfOutputLength) {}
-        rubberband_retrieve(rState, secondHalfOutputPtr.ptr, secondHalfOutputLength);
-        rubberband_delete(rState);
-
-        sample_t[] outputBuffer = new sample_t[]((firstHalfOutputLength + secondHalfOutputLength) * nChannels);
-        if(linkChannels) {
-            foreach(channels_t channelIndex, channel; firstHalfOutputChannels) {
-                foreach(i, sample; channel) {
-                    outputBuffer[i * nChannels + channelIndex] = sample;
-                }
-            }
-            auto secondHalfOffset = firstHalfOutputLength * nChannels;
-            foreach(channels_t channelIndex, channel; secondHalfOutputChannels) {
-                foreach(i, sample; channel) {
-                    outputBuffer[secondHalfOffset + i * nChannels + channelIndex] = sample;
-                }
-            }
-        }
-        else {
-            auto firstHalfOffset = localStartFrame * nChannels;
-            foreach(i, sample; firstHalfOutputChannels[0]) {
-                for(channels_t channelIndex = 0; channelIndex < nChannels; ++channelIndex) {
-                    if(channelIndex == singleChannelIndex) {
-                        outputBuffer[i * nChannels + channelIndex] = sample;
+            if(linkChannels) {
+                foreach(channels_t channelIndex, channel; firstHalfChannels) {
+                    foreach(i, ref sample; channel) {
+                        sample = _audioSeq[(localStartFrame + i) * nChannels + channelIndex];
                     }
-                    else {
-                        outputBuffer[i * nChannels + channelIndex] =
-                            _audioSeq[firstHalfOffset + i * nChannels + channelIndex];
+                }
+                foreach(channels_t channelIndex, channel; secondHalfChannels) {
+                    foreach(i, ref sample; channel) {
+                        sample = _audioSeq[(localSrcFrame + i) * nChannels + channelIndex];
                     }
                 }
             }
-            auto secondHalfOutputOffset = firstHalfOutputLength * nChannels;
-            auto secondHalfOffset = firstHalfOffset + secondHalfOutputOffset;
-            foreach(i, sample; secondHalfOutputChannels[0]) {
-                for(channels_t channelIndex = 0; channelIndex < nChannels; ++channelIndex) {
-                    if(channelIndex == singleChannelIndex) {
-                        outputBuffer[secondHalfOutputOffset + i * nChannels + channelIndex] = sample;
-                    }
-                    else {
-                        outputBuffer[secondHalfOutputOffset + i * nChannels + channelIndex] =
-                            _audioSeq[secondHalfOffset + i * nChannels + channelIndex];
-                    }
+            else {
+                foreach(i, ref sample; firstHalfChannels[0]) {
+                    sample = _audioSeq[(localStartFrame + i) * nChannels + singleChannelIndex];
                 }
-            }
-        }
-
-        auto immutable removeStartIndex = clamp(localStartFrame * nChannels,
-                                                0,
-                                                _audioSeq.currentPieceTable.logicalLength);
-        auto immutable removeEndIndex = clamp(localEndFrame * nChannels,
-                                              removeStartIndex,
-                                              _audioSeq.currentPieceTable.logicalLength);
-        auto pieceTable = _audioSeq.currentPieceTable.
-            remove(removeStartIndex, removeEndIndex).
-            insert(AudioSegment(outputBuffer, nChannels), removeStartIndex);
-        _audioSeq.appendToHistory(pieceTable);
-        _nframes = cast(nframes_t)(_audioSeq.length / nChannels);
-    }
-
-    // stretch the audio such that the frame at localSrcFrame becomes the frame at localDestFrame
-    // if linkChannels is true, perform the stretch for all channels simultaneously, ignoring channelIndex
-    void stretchThreePointFromSource(nframes_t localStartFrame,
-                                     nframes_t localSrcFrame,
-                                     nframes_t localDestFrame,
-                                     nframes_t localEndFrame,
-                                     AudioSequence.PieceTable leftSource,
-                                     AudioSequence.PieceTable rightSource,
-                                     bool linkChannels = false,
-                                     channels_t singleChannelIndex = 0) {
-        immutable channels_t stretchNChannels = linkChannels ? nChannels : 1;
-
-        auto immutable removeStartIndex = clamp(localStartFrame * nChannels,
-                                                0,
-                                                _audioSeq.currentPieceTable.logicalLength);
-        auto immutable removeEndIndex = clamp(localEndFrame * nChannels,
-                                              removeStartIndex,
-                                              _audioSeq.currentPieceTable.logicalLength);
-
-        immutable double firstScaleFactor = (localSrcFrame > localStartFrame) ?
-            (cast(double)(localDestFrame - localStartFrame) /
-             cast(double)(leftSource.length / nChannels)) : 0;
-        immutable double secondScaleFactor = (localEndFrame > localSrcFrame) ?
-            (cast(double)(localEndFrame - localDestFrame) /
-             cast(double)(rightSource.length / nChannels)) : 0;
-
-        localStartFrame = 0;
-        localSrcFrame = (localStartFrame < localSrcFrame) ? localSrcFrame - localStartFrame : 0;
-        localDestFrame = (localStartFrame < localDestFrame) ? localDestFrame - localStartFrame : 0;
-        localEndFrame = (localStartFrame < localEndFrame) ? localEndFrame - localStartFrame : 0;
-
-        uint firstHalfLength = cast(uint)(leftSource.length / nChannels);
-        uint secondHalfLength = cast(uint)(rightSource.length / nChannels);
-        ScopedArray!(float[][]) firstHalfChannels = new float[][](stretchNChannels);
-        ScopedArray!(float[][]) secondHalfChannels = new float[][](stretchNChannels);
-        ScopedArray!(float*[]) firstHalfPtr = new float*[](stretchNChannels);
-        ScopedArray!(float*[]) secondHalfPtr = new float*[](stretchNChannels);
-        for(auto i = 0; i < stretchNChannels; ++i) {
-            float[] firstHalf = new float[](firstHalfLength);
-            float[] secondHalf = new float[](secondHalfLength);
-            firstHalfChannels[i] = firstHalf;
-            secondHalfChannels[i] = secondHalf;
-            firstHalfPtr[i] = firstHalf.ptr;
-            secondHalfPtr[i] = secondHalf.ptr;
-        }
-
-        if(linkChannels) {
-            foreach(channels_t channelIndex, channel; firstHalfChannels) {
-                foreach(i, ref sample; channel) {
-                    sample = leftSource[i * nChannels + channelIndex];
+                foreach(i, ref sample; secondHalfChannels[0]) {
+                    sample = _audioSeq[(localSrcFrame + i) * nChannels + singleChannelIndex];
                 }
-            }
-            foreach(channels_t channelIndex, channel; secondHalfChannels) {
-                foreach(i, ref sample; channel) {
-                    sample = rightSource[i * nChannels + channelIndex];
-                }
-            }
-        }
-        else {
-            foreach(i, ref sample; firstHalfChannels[0]) {
-                sample = leftSource[i * nChannels + singleChannelIndex];
-            }
-            foreach(i, ref sample; secondHalfChannels[0]) {
-                sample = rightSource[i * nChannels + singleChannelIndex];
             }
         }
 
@@ -4766,14 +4648,14 @@ private:
                         OnsetSequence onsets = _editRegion.linkChannels ?
                             _editRegion._onsetsLinked : _editRegion._onsets[_moveOnsetChannel];
                         if(onsets[_moveOnsetIndex].leftSource && onsets[_moveOnsetIndex].rightSource) {
-                            _editRegion.region.stretchThreePointFromSource(onsetFrameStart,
-                                                                           _moveOnsetFrameSrc,
-                                                                           _moveOnsetFrameDest,
-                                                                           onsetFrameEnd,
-                                                                           onsets[_moveOnsetIndex].leftSource,
-                                                                           onsets[_moveOnsetIndex].rightSource,
-                                                                           _editRegion.linkChannels,
-                                                                           _moveOnsetChannel);
+                            _editRegion.region.stretchThreePoint(onsetFrameStart,
+                                                                 _moveOnsetFrameSrc,
+                                                                 _moveOnsetFrameDest,
+                                                                 onsetFrameEnd,
+                                                                 _editRegion.linkChannels,
+                                                                 _moveOnsetChannel,
+                                                                 onsets[_moveOnsetIndex].leftSource,
+                                                                 onsets[_moveOnsetIndex].rightSource);
                         }
                         else {
                             _editRegion.region.stretchThreePoint(onsetFrameStart,
